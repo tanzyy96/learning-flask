dd8679e5b59f377635efa82e9caf2ff5e3c9c9b0
tanzy <tanz0142@e.ntu.edu.sg>
2018-10-19 11:18:19 +0800

    About Page

--- routes.py
+++ routes.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)
+
+#source venv/bin/activate
+#python routes.py
